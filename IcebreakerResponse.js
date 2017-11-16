cc165e40a3e682781671ccc70cb02075007a7ed9
Anastassia Troitskaia <atroitskaia@gmail.com>
2017-11-16 19:25:39 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,10 @@
-function IcebreakerResponse(email){
+function IcebreakerResponse(email) {
   this.email = email
+}
+
+IcebreakerResponse.BatchCreate = function(emails) {
+  return emails.map(function(userData) {
+    return new IcebreakerResponse(userDataata[0])
+  })
 }
 
